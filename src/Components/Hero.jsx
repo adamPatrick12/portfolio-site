3454348b1a9de3a22f788a521dae6cc34f21bb9a
adamPatrick12 <adam_77paper@hotmail.com>
2023-01-11 17:53:29 -0500

    Update hero

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -1,40 +1,29 @@
-import "../Styles/Hero.css"
-import Memoji from "../Images/character.png"
+import "../Styles/Hero.css";
+import Memoji from "../Images/character.png";
 
 const Hero = () => {
-    return (
-        <div className="HeroContainer">
-            <div className="HeroTextContainer">
-                <div className="WaveContainer">
-                    <div className="Greeting">
-                        Hey There!
-                    </div>
-                    <div className="Wave">
-                    👋🏽
-                    </div>
-                </div>
-                <div className="APContainer">
-                    <div className="IM">
-                    I'M
-                    </div>
-                    <div className="AP">
-                    ADAM PATRICK
-                    </div>
-                </div>
-                <div className="About">
-                A software developer 
-                based in Toronto. I have 
-                great interest in Front-end development, 
-                Game development, Blockchian technology, 
-                and everything in between.
-                </div>
-            </div>
-            <div className="EmojiContainer">
-                <img className="Memoji" src={Memoji} alt="" />
-            </div>
+  return (
+    <div className="HeroContainer">
+      <div className="HeroTextContainer">
+        <div className="WaveContainer">
+          <div className="Greeting">Hey There!</div>
+          <div className="Wave">👋🏽</div>
         </div>
-    )
-}
+        <div className="APContainer">
+          <div className="IM">I'M</div>
+          <div className="AP">ADAM PATRICK</div>
+        </div>
+        <div className="About">
+          A software developer based in Toronto. I have great interest in
+          Front-end development, Game development, WebGL technology, and
+          everything in between.
+        </div>
+      </div>
+      <div className="EmojiContainer">
+        <img className="Memoji" src={Memoji} alt="" />
+      </div>
+    </div>
+  );
+};
 
-export default Hero
-
+export default Hero;
