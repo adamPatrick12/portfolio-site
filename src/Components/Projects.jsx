f367b0d48ec4c996b2b7312cac99077bc36c78a1
adamPatrick12 <adam_77paper@hotmail.com>
2023-05-12 19:14:41 -0400

    Updated project

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -1,12 +1,12 @@
 import "../Styles/projects.css";
 import { SiGithub } from "react-icons/si";
 import { SiFastapi } from "react-icons/si";
-import { SiHiveBlockchain } from "react-icons/si";
+import { TiSocialInstagram } from "react-icons/ti";
 import { TiWeatherStormy } from "react-icons/ti";
 import { FaHeadphonesAlt } from "react-icons/fa";
 import WeatherIMG from "../Images/weather.png";
 import StudyBeatsIMG from "../Images/studybeatsfm.png";
-import NFTStoreIMG from "../Images/nftstore.png";
+import InstagramCloneIMG from "../Images/instagramClone.png";
 
 const Projects = () => {
   return (
@@ -17,6 +17,44 @@ const Projects = () => {
       </div>
 
       <div className="projectsContainer">
+        <div className="Project">
+          <div className="ProjectTextContainer">
+            <div className="NFT-Title">
+              Instagram Clone <TiSocialInstagram size="0.7em" />
+            </div>
+            <div className="ProjectDesc">
+              A Full Stack clone of Instagram's features and a complete redesign
+              of the UI. Built using React with TypeScript for the frontend,
+              NodeJS for the backend, MongoDB for the database, and AWS S3 to
+              store images.
+            </div>
+            <div className="ProjectButton">
+              <button
+                onClick={() =>
+                  window.open("https://adampatrick12.vercel.app/", "_blank")
+                }
+                className="DemoBTN"
+              >
+                Live Demo <SiFastapi className="Git" size="0.9em" />{" "}
+              </button>
+              <button
+                onClick={() =>
+                  window.open(
+                    "https://github.com/adamPatrick12/instagram-clone",
+                    "_blank"
+                  )
+                }
+                className="CodeBTN"
+              >
+                {" "}
+                View Code <SiGithub className="Git" size="0.9em" />{" "}
+              </button>
+            </div>
+          </div>
+          <div className="ProjectIMGContainer">
+            <img className="ProjectIMG" src={InstagramCloneIMG} alt="" />
+          </div>
+        </div>
         <div className="Project">
           <div className="ProjectTextContainer">
             <div className="ProjectTitle">
@@ -55,47 +93,7 @@ const Projects = () => {
             <img className="ProjectIMG" src={StudyBeatsIMG} alt="" />
           </div>
         </div>
-        <div className="Project">
-          <div className="ProjectTextContainer">
-            <div className="NFT-Title">
-              NFT Store <SiHiveBlockchain size="0.7em" />
-            </div>
-            <div className="ProjectDesc">
-              An app that provides various NFT data like the market cap, number
-              of owners, 7-day trading volume, and the floor price for various
-              NFT collections. Built with React, Framer Motion and the Opensea
-              API
-            </div>
-            <div className="ProjectButton">
-              <button
-                onClick={() =>
-                  window.open(
-                    "https://adampatrick12.github.io/nft-shop/#/items",
-                    "_blank"
-                  )
-                }
-                className="DemoBTN"
-              >
-                Live Demo <SiFastapi className="Git" size="0.9em" />{" "}
-              </button>
-              <button
-                onClick={() =>
-                  window.open(
-                    "https://github.com/adamPatrick12/nft-shop",
-                    "_blank"
-                  )
-                }
-                className="CodeBTN"
-              >
-                {" "}
-                View Code <SiGithub className="Git" size="0.9em" />{" "}
-              </button>
-            </div>
-          </div>
-          <div className="ProjectIMGContainer">
-            <img className="ProjectIMG" src={NFTStoreIMG} alt="" />
-          </div>
-        </div>
+
         <div className="Project">
           <div className="ProjectTextContainer">
             <div className="WeatherTitle">
